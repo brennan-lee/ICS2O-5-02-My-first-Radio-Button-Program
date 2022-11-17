7149a7381677d8602a15ce64a94699d0fa3b5064
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-17 13:44:31 -0500

    5-02

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Brennan Lee All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Brennan lee
+// Created on: nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * This function updates the slider value.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
-  })
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
 }
 
 /**
- * This function displays an alert.
+ * This function displays the slider value.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  buttonOnChecked = document.getElementById('flash1').checked
+
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is: 1</p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is: 6</p>"
+  }
 }
